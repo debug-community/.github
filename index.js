6212e475cdefcf6d877aa438c2afa1d7833cedb9
dc-choi <ddagae0805@gmail.com>
2025-05-24 14:26:25 +0900

    content 추가

--- index.js
+++ index.js
@@ -30,9 +30,33 @@ async function generateReadme() {
   )
   .join('\n');
 
-  const readmeContent = `## 👥 Organization Members
+  const readmeContent = `
+  ## Hi there 👋
 
+  <!--
+
+  **Here are some ideas to get you started:**
+
+  🙋‍♀️ A short introduction - what is your organization all about?
+  🌈 Contribution guidelines - how can the community get involved?
+  👩‍💻 Useful resources - where can the community find your docs? Is there anything else the community should know?
+  🍿 Fun facts - what does your team eat for breakfast?
+  🧙 Remember, you can do mighty things with the power of [Markdown](https://docs.github.com/github/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax)
+  -->
+  Debug 커뮤니티의 GitHub Organization 입니다.
+
+  ## 👥 Organization Members
   ${avatars}
+
+  ## Want to join?
+  <a href="https://discord.gg/7sAYdbff">
+    <img src="https://skillicons.dev/icons?i=discord" width="32" height="32" alt="Discord"/>
+  </a>
+
+  ## contect
+  <a href="mailto:debug331@gmail.com">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/4/4e/Gmail_Icon.png" width="32" height="32" alt="Gmail"/>
+  </a>
   `;
 
   fs.writeFileSync('README.md', readmeContent);
